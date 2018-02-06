20ea6e7696f7c2a23b71d8f38699cf979fc5d5d3
jakubroczniewski <jakub.roczniewski@gmail.com>
2018-02-06 09:00:33 +0100

    mysql_repair - zmiana sposobu obslugi orkana99

--- jroczniewski/bin/troj_mysql_repair.sh
+++ jroczniewski/bin/troj_mysql_repair.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for x in {1..5}; do
+for x in {1..6}; do
+  if [ "${x}" -eq "6" ]; then
+    x=99
+  fi
   echo repairing orkan${x}
   ssh jroot@orkan${x} '/root/bin/make_sync_sql.sh | tee /home/jroot/sync/$(date +"%Y%m%d").sql | mysql ; echo SYNCED > /tmp/checksum_status ; chown dbascripts:dbascripts /tmp/checksum_status'
 done
-x=99
-echo repairing orkan${x}
-ssh jroot@orkan${x} '/root/bin/make_sync_sql.sh | tee /home/jroot/sync/$(date +"%Y%m%d").sql | mysql ; echo SYNCED > /tmp/checksum_status ; chown dbascripts:dbascripts /tmp/checksum_status'
